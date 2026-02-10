6dc5a537b7e5cdfebe8f873fea7ee2ea9547e0f5
hnidesu <hnidesu@outlook.com>
2026-02-10 20:14:05 +0800

    feat: add settings option to edit .env file

--- src/view/note_stager_setting_view.ts
+++ src/view/note_stager_setting_view.ts
@@ -1,5 +1,8 @@
 import NoteStagePlugin from "main";
-import { App, PluginSettingTab, Setting } from "obsidian";
+import { App, Notice, PluginSettingTab, Setting } from "obsidian";
+import * as path from "path"
+import * as fs from "fs"
+import * as child_process from "child_process";
 
 export class NoteStagerSettingView extends PluginSettingTab {
 	private _plugin: NoteStagePlugin;
@@ -18,5 +21,26 @@ export class NoteStagerSettingView extends PluginSettingTab {
 					await this._plugin.saveSettings();
 				});
 		});
+
+		new Setting(containerEl)
+			.setName("Environment Variables")
+			.setDesc(
+				"Commands can load environment variables from a .env file in the plugin directory."
+			)
+			.addButton((btn) => {
+				btn.setButtonText("Edit")
+					.onClick(() => {
+						const envFilePath = path.resolve((this._plugin.app.vault.adapter as any).basePath,this._plugin.manifest.dir!, ".env")
+						// Create file if missing
+						if (!fs.existsSync(envFilePath))
+							fs.writeFileSync(`"${envFilePath}"`, "", "utf-8");
+						child_process.exec(envFilePath, (err) => {
+							if (err) {
+								new Notice("Failed to open .env file");
+								console.error(err);
+							}
+						});
+					});
+			});
 	}
 }
